0b9be5f33158f4ddda6df320105e74ce74cb6da5
Devesh Gupta <devesh.gupta.198@gmail.com>
2025-02-04 11:11:30 +0530

    Header change.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         Demo Project
+        <div className="Head-description">Demo data dashboard(An example of data represantation of interactive charts and table with sorting, pagination, CSV export and search.)</div>
       </header>
       <Home/>
     </div>
